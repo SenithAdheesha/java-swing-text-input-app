08a2ed74095523ab75fda0ee385849305f4f5957
Senith Adheesha <senithadeesha7@gmail.com>
2026-03-17 17:25:13 +0530

    Add files via upload

---
+++ App1/App1.java
@@ -0,0 +1,59 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class App1 extends JFrame {
+
+    JTextField txtName;
+    JLabel lblDisplay;
+
+    App1() {
+
+        setTitle("Type & Click");
+        setSize(450, 150);
+        setLocation(200, 200);
+
+        Container con = getContentPane();
+        FlowLayout flow = new FlowLayout();
+        con.setLayout(flow);
+
+        txtName = new JTextField(20);
+        lblDisplay = new JLabel("Type and Click");
+        JButton btnShow = new JButton("Show");
+
+
+        btnShow.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                showName(e);
+            }
+        });
+
+
+        con.add(txtName);
+        con.add(btnShow);
+        con.add(lblDisplay);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    public void showName(ActionEvent evt) {
+        
+        String name = txtName.getText();
+        System.out.println(name);  
+        lblDisplay.setText( name );
+    }
+
+    public static void main(String args[]) {
+        App1 app = new App1();
+    }
+
+}
